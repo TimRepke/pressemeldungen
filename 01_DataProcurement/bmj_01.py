260d099c975bcd58de4ee02b72c6505812a8ac2d
Tim Repke <repke@mcc-berlin.net>
2024-01-23 17:23:18 +0100

    mbj

---
+++ 01_DataProcurement/bmj_01.py
@@ -0,0 +1,48 @@
+import logging
+from typing import Any, Iterable
+
+from scrapy import Request, Spider
+from scrapy.http import Response
+
+from util import get_stripped, get_all_stripped
+
+logger = logging.getLogger('bmj')
+
+BASE_URL = 'https://www.bmj.de'
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+class BMJSpider(Spider):
+    name = 'bmj'
+    start_urls = [f'{BASE_URL}/SiteGlobals/Forms/Dokumenttypsuche/Dokumenttypsuche_Formular.html?nn=148026'
+                  f'&folderInclude.HASH=eed2fpFDtvtyioHOwVz6jK_tIh4zwSc%3D&folderExclude=-%2FBMJ%2F*%2FRestricted'
+                  f'&defaultFilter=documentType_%3APressRelease&defaultFilter.HASH=7f8aqx8Bv4VOx999EgpiDtKxIGzL7E0%3D'
+                  f'&callerId.HASH=818bKvjZjjdad0vcaEpNJaQbOhKvnok%3D'
+                  f'&folderExclude.HASH=45eapg3qTQ140sxaYkSoqXsuauF-7XE%3D'
+                  f'&folderInclude=%2FBMJ%2FDE%2F*+%2FBMJ%2FEN%2F*+%2FBMJ%2FSharedDocs%2F*&callerId=110490']
+
+    def start_requests(self) -> Iterable[Request]:
+        for url in self.start_urls:
+            yield Request(url=url, callback=self.parse_list, dont_filter=True, headers=HEADERS)
+
+    def parse_list(self, response: Response):
+        for link in response.css('a.c-teaser__link::attr("href")').getall():
+            logger.debug(f'Found article link: {link}')
+            yield response.follow(f'{BASE_URL}/{link}', self.parse_article, headers=HEADERS)
+
+        next_link = response.css('nav.c-nav-index li.c-nav-index__item--next > a::attr("href")').get()
+        if next_link is not None:
+            logger.info(f'Found next page: {next_link}')
+            yield response.follow(f'{BASE_URL}/{next_link}', self.parse_list, headers=HEADERS)
+
+    def parse_article(self, response: Response, **kwargs: Any):
+        logger.debug(f'Parsing article page: {response.request.url}')
+        content = response.css('#content')
+
+        yield {
+            'date': get_stripped(content, '.c-meta-info > time::attr("datetime")')[:10],
+            'descriptor': 'Pressemitteilung',
+            'title': get_all_stripped(content, 'h1.c-page-intro__headline *::text'),
+            'teaser': get_all_stripped(content, '.c-page-intro__text *::text'),
+            'text': get_all_stripped(content, '.s-richtext *::text'),
+            'link': response.request.url
+        }
--- README.md
+++ README.md
@@ -15,4 +15,24 @@ $ python -m scrapy runspider bmuv_02.py -o ../data/raw/articles_bmuv_current.jso
 $ python -m scrapy runspider bmwk_01.py -o ../data/raw/articles_bmwk.jsonl
 $ python -m scrapy runspider bmwsb_01.py -o ../data/raw/articles_bmwsb.jsonl
 $ python -m scrapy runspider reg_01.py -o ../data/raw/articles_reg.jsonl
+$ python -m scrapy runspider bmj_01.py -o ../data/raw/articles_bmj.jsonl
+
+BMAS
+https://www.bmas.de/SiteGlobals/Forms/Suche/Aktuelles-Suche_Formular.html?dateOfIssue_dt=2020-12-31T23%3A00%3A00Z+2021-12-31T23%3A00%3A00Z+2022-12-31T23%3A00%3A00Z&documentType_=pressrelease&showNoGesetzesstatus=true&showNoStatus=true
+
+BMVg
+https://www.bmvg.de/de/presse/alle-pressetermine-pressemitteilungen-bmvg
+
+BMFSJF
+https://www.bmfsfj.de/bmfsfj/aktuelles/presse/pressemitteilungen/106404!search?state=H4sIAAAAAAAAAFWOPQ-CMBCG_4q5uQNEg9DNoMyYsBmGBg5tUltsD78I_91SHWS797mvZ4RWEBbWXIHrQSkWcmX-09yt8EnLiX8yONw9hG33vgO8E8phgIc7alrAXgmpC6kI7XFAK9EBP9UMOtEg-XqE9TbbpB5CnCZJFkE9MbhIciXaUpz9pThicPO7L-AADN6yz02L3-CM9VLBcNWiayBozAa50Y6sf04_l-kDs-ug0_sAAAA%3D&hitsPerPage=20#search106404
+
+BMG
+https://www.bundesgesundheitsministerium.de/presse/pressemitteilungen.html
+
+BMBF
+https://www.bmbf.de/SiteGlobals/Forms/bmbf/suche/pressemitteilungen/Pressemitteilungensuche_Formular.html?gtp=33424_list%253D33&resultsPerPage=50#searchResults
+
+BMZ
+https://www.bmz.de/de/aktuelles/aktuelle-meldungen
+https://www.bmz.de/de/aktuelles/archiv-aktuelle-meldungen
 ```
