34a7b449935c14ee53e1396d2786f772176ca127
Tim Repke <repke@mcc-berlin.net>
2024-01-23 22:27:48 +0100

    minor changes all around

--- 02_Exploration/compounds.py
+++ 02_Exploration/compounds.py
@@ -6,11 +6,9 @@ import pandas as pd
 p = re.compile(r'klima(\w+)')
 REGEX = re.compile(r'articles_(.+)\.jsonl')
 
-
 files = Path().glob(pattern='../data/raw/*')
 
 res = []
-
 
 for file in files:
     fname = str(file)
@@ -31,4 +29,4 @@ g1 = df.groupby(['compound']).count()
 g2 = df.groupby(['ministry', 'compound']).count()
 
 g1['dummy'].sort_values().to_csv('../data/compounds.csv')
-g2.sort_values(['ministry','dummy']).to_csv('../data/compounds_grouped.csv')
+g2.sort_values(['ministry', 'dummy']).to_csv('../data/compounds_grouped.csv')
